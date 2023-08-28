a2f592c9bfa7b84a1034cd280f31473d8e89b836
Marcel Heers <info@heers.it>
2023-08-28 21:33:10 +0300

    adds a very first implementation of the tasks.task rest api

---
+++ client/task_tasks.go
@@ -0,0 +1,43 @@
+package client
+
+import "github.com/nightwriter/go-bitrix/types"
+
+func (c *Client) TasksTaskList(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("tasks.task.list", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) TasksTaskGet(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("tasks.task.get", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) TasksTaskAdd(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("tasks.task.add", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) TasksTaskUpdate(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("tasks.task.update", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) TasksTaskDelete(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("tasks.task.delete", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
